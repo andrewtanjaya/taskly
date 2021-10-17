b38c41682298a1f5cd684a717f26ee0a13593fa8
andrewtanjaya <andrewtanjaya21@gmail.com>
2021-10-18 00:06:30 +0700

    logout button

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import HomeContent from '../HomeContent/HomeContent'
 import {RiMenu2Fill} from 'react-icons/ri'
 import './Home.css'
+import { useAuth } from '../Context/AuthContext'
+
+
 
 function Home() {
+    const {logout } = useAuth()
     return (
         <div className="homeBg">
             <div className="miniMenu">
@@ -14,9 +18,9 @@ function Home() {
                 <HomeContent/>
             </div>
 
-{/*             
-            <pre>{JSON.stringify(currentUser, null, 2)}</pre>
-            <button onClick={(e)=>logout()}> Logout</button> */}
+            
+            {/* <pre>{JSON.stringify(currentUser, null, 2)}</pre> */}
+            <button onClick={(e)=>logout()}> Logout</button>
 
         </div>
     )
